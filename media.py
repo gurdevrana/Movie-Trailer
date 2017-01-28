1dcc1db562f5213d0920c6efdf802e08c095b73b
gurdevrana <gurdev.rana@gmail.com>
2017-01-29 01:39:48 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,15 +1,14 @@
 class movie():
     """ the following function used for intializing the objects attributes """
-    def __init__(self,movie_title,movie_storyline,poster_image,trailer):
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer):
         # setting value of title to value passed
-        self.title=movie_title
+        self.title = movie_title
 
         # setting value of storyline to value passed
-        self.storyline=movie_storyline
+        self.storyline = movie_storyline
 
         # setting value of poster_url to value passed
-        self.poster_image_url=poster_image
+        self.poster_image_url = poster_image
 
         # setting value of url to value passed
-        self.trailer_youtube_url=trailer
-
+        self.trailer_youtube_url = trailer
